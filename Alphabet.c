c69af3ecb5d14fe4efeb10f35a3674950ca1b596
Sadikaislamtisha <107317118+Sadikaislamtisha@users.noreply.github.com>
2022-06-25 03:13:27 +0600

    Add files via upload

---
+++ Alphabet.c
@@ -0,0 +1,25 @@
+/*Sadika Islam Tisha
+Id:221-35-872*/
+#include<stdio.h>
+int main()
+{
+    char c;
+
+
+    printf("Enter a character:");
+    scanf("%c",&c);
+
+    if((c>='a' && c<='z')||(c>='A' && c<='Z'))
+    {
+        printf("%c is an alphabet\n",c);
+    }
+    else
+    {
+        printf("%c is not an alphabet\n",c);
+    }
+
+
+return 0;
+
+
+}
